ca728a19af9eb7dbb9910e8edff98e4b4fad494e
徐鼎 <221900234@smail.nju.edu.cn>
2026-08-29 15:51:14 +0800

    fix: 更新配置允许访问E:\\mimo\\test目录

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ load_dotenv()
 # LLM配置
 LLM_API_BASE = os.getenv("LLM_API_BASE", "http://127.0.0.1:8788")
 LLM_API_PATH = os.getenv("LLM_API_PATH", "/v1/responses")
-LLM_MODEL_NAME = os.getenv("LLM_MODEL_NAME", "defalut")
+LLM_MODEL_NAME = os.getenv("LLM_MODEL_NAME", "mimo-v2.5-pro")
 LLM_TIMEOUT = int(os.getenv("LLM_TIMEOUT", "60"))
 LLM_MAX_RETRIES = int(os.getenv("LLM_MAX_RETRIES", "3"))
 
 # 工具配置
 PYTHON_EXEC_TIMEOUT = int(os.getenv("PYTHON_EXEC_TIMEOUT", "10"))
-FILE_IO_ALLOWED_PATHS = os.getenv("FILE_IO_ALLOWED_PATHS", ".").split(",")
+FILE_IO_ALLOWED_PATHS = os.getenv("FILE_IO_ALLOWED_PATHS", ".,E:\\mimo\\test").split(",")
 
 # 记忆配置
 MEMORY_CONTEXT_WINDOW = int(os.getenv("MEMORY_CONTEXT_WINDOW", "10"))
